6ddbb6140e246f935c94b651286c121775caaa25
unknown <omkardp13@gmail.com>
2026-02-22 19:11:22 -0800

    first sql server learings commit

---
+++ BasicComponents/BASIC.sql
@@ -0,0 +1,22 @@
+/*
+Use below command to switch your prefered database
+Syntax USE <database_name>
+*/
+
+USE MyDatabase;
+
+/* Commentes-Notes to explain code,ignored during execution
+
+-- This is inline comment
+
+/*
+THis
+is 
+multiline
+commnet
+*/
+
+*/
+
+--If you write value and if it is containes charactors so you need to put it in single quote('')
+
